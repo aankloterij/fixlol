1806aaf10a70330c20effd46b70f3d6be591d8bc
Holland Webweek <Holland Webweek>
2016-04-09 20:42:14 +0200

    fixlol

---
+++ app/Fix.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+class Fix extends Eloquent
+{
+	protected $fillable = [
+		'subject', 'body', 'tags'
+	];
+
+	public function setBodyAttribute($value)
+	{
+		$this->attributes['body'] = e($value);
+
+		preg_match('/#([a-zA-Z-_][a-zA-Z0-9-_]*)/', $value, $tags);
+
+		array_shift($tags);
+
+		array_map(function ($value) {
+			return e($value);
+		}, $value);
+
+		$this->attirbutes['tags'] = implode('|', $tags);
+	}
+}
