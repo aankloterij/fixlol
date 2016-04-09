2cc82a3ddc91380c6db8e65e83018c806bfac880
Sibren Talens <sibrentalens@gmail.com>
2016-04-09 23:05:58 +0200

    fixlol

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+    <link rel="shortcut icon" type="image/x-icon" href="{{ url('/favicon.ico') }}">
     <link rel="stylesheet" href="{{ url('/css/main.css') }}">
     <link rel="stylesheet" href="{{ url('/css/auth.css') }}">
     @yield('head')
