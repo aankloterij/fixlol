9bc1fa5e5482dfaa85c68b89e38365eff639f158
Max Verbeek <m4xv3rb33k@gmail.com>
2016-04-10 06:05:10 +0200

    fixlol

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,9 +1,14 @@
 @extends('layouts.dashboard')
 
 @section('head')
+	<link rel="stylesheet" href="{{ url('/css/profile.css') }}">
 	<title>{{ $user->name }}</title>
 @endsection
 
 @section('body')
-
+	<div id="profile">
+		<img src="" alt="">
+		<h1>{{ $user->name }}</h1>
+		<h2>{{ $user->username }}</h2>
+	</div>
 @endsection
