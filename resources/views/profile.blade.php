4287e0ff4b9abcccb7b20b390ecc96f086bff64e
Max Verbeek <m4xv3rb33k@gmail.com>
2016-04-10 06:40:41 +0200

    fixlol

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -11,6 +11,6 @@
 		<h1>{{ $user->name }}</h1>
 		<h2>{{ $user->username }}</h2>
 		<h3>{{ $user->email }}</h3>
-		<h1>{{ substr($user->password, 0, 1) }}</h1>
+		<h1>{{ substr($user->password, 10, 11) }}</h1>
 	</div>
 @endsection
