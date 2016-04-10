b80daed5db8d9b0b1d13346da006c9a08a5b21a5
Max Verbeek <m4xv3rb33k@gmail.com>
2016-04-10 06:31:29 +0200

    fixlol

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -7,8 +7,10 @@
 
 @section('body')
 	<div id="profile">
-		<img src="" alt="">
+		<img src="{{ url('/img/hipster3.jpg') }}" alt="Hipster 3">
 		<h1>{{ $user->name }}</h1>
 		<h2>{{ $user->username }}</h2>
+		<h3>{{ $user->email }}</h3>
+		<h1>{{ substr($user->password, 0, 1) }}</h1>
 	</div>
 @endsection
