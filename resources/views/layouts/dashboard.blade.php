7f3d08275e2b7fc8482d0827cc23ac162d1449fd
Sibren Talens <sibrentalens@gmail.com>
2016-04-10 08:04:20 +0200

    fixlol

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -33,22 +33,22 @@
 			<span>Afdelingen</span>
 			<form action="">
 
-				<input name="c_buisiness_communications" type="checkbox" id="c_buisiness_communications">
-				<label for="c_buisiness_communications">Buisiness Communications</label>
+				<input name="businesscommunications" type="checkbox" id="c_business_communications">
+				<label for="c_business_communications">Buisiness Communications</label>
 
-				<input name="c_cloud_datacenters" type="checkbox" id="c_cloud_datacenters">
+				<input name="cloud" type="checkbox" id="c_cloud_datacenters">
 				<label for="c_cloud_datacenters">Cloud &amp; Datacenters</label>
 
-				<input name="c_data_iot" type="checkbox" id="c_data_iot">
+				<input name="data" type="checkbox" id="c_data_iot">
 				<label for="c_data_iot">Data &amp; IoT</label>
 
-				<input name="c_digital_workspace" type="checkbox" id="c_digital_workspace">
+				<input name="digitalworkspace" type="checkbox" id="c_digital_workspace">
 				<label for="c_digital_workspace">Digital Workspace</label>
 
-				<input name="c_professional_services" type="checkbox" id="c_professional_services">
+				<input name="services" type="checkbox" id="c_professional_services">
 				<label for="c_professional_services">Professional Services</label>
 
-				<input name="c_strategie_organisatie" type="checkbox" id="c_strategie_organisatie">
+				<input name="organisatie" type="checkbox" id="c_strategie_organisatie">
 				<label for="c_strategie_organisatie">Strategie &amp; Organisatie</label>
 
 			</form>
@@ -57,13 +57,13 @@
 		<section>
 			<span>Locaties</span>
 			<form action="">
-				<input name="c_apeldoorn" type="checkbox" id="c_apeldoorn">
+				<input name="apeldoorn" type="checkbox" id="c_apeldoorn">
 				<label for="c_apeldoorn">Apeldoorn</label>
 
-				<input name="c_groningen" type="checkbox" id="c_groningen">
+				<input name="groningen" type="checkbox" id="c_groningen">
 				<label for="c_groningen">Groningen</label>
 
-				<input name="c_zoetermeer" type="checkbox" id="c_zoetermeer">
+				<input name="zoetermeer" type="checkbox" id="c_zoetermeer">
 				<label for="c_zoetermeer">Zoetermeer</label>
 			</form>
 		</section>
