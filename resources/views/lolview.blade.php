303b7cb403a6ab812feefdb9246b1d7ed4e19ef4
Max Verbeek <m4xv3rb33k@gmail.com>
2016-04-10 09:39:24 +0200

    fixlol

--- resources/views/lolview.blade.php
+++ resources/views/lolview.blade.php
@@ -6,8 +6,8 @@
 
 @section('body')
 	<div id="message">
-		<h2>$message['from']</h2>
-		<h1>$message['title']</h1>
-		<p>$message['stuff']</p>
+		<h2>{{ $message['from'] }}</h2>
+		<h1>{{ $message['title'] }}</h1>
+		<p>{{ $message['stuff'] }}</p>
 	</div>
 @endsection
